3b287d7bbeea1cc4db388c32de642d3001a3b85e
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-05-31 14:37:45 -0400

    Adds searches controller with index method, facade, poro

---
+++ app/controllers/api/v1/searches_controller.rb
@@ -0,0 +1,13 @@
+class SearchesController < ApplicationController
+  def index
+    if params[:query].present? && params[:video_duration].present?
+      query_keywords = params[:query]
+      video_duration = params[:video_duration]
+      response = YoutubeService.search(query_keywords, video_duration)
+      @videos = YoutubeService.parse_video_response(response)
+      render json: @videos
+    else
+      render json: { error: "Query and video duration parameters are required" }, status: :bad_request
+    end
+  end
+end
---
+++ app/poros/youtube_video.rb
@@ -0,0 +1,8 @@
+class YoutubeVideo
+  attr_reader :title, :url
+
+  def initialize(title:, url:)
+    @title = title
+    @url = url
+  end
+end
