2822f6fcbde735fe642f31ff248e6d648b1c19a3
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-02 15:17:21 -0400

    Modifies searches controller name for the correct path API::V1::

--- config/initializers/google_token_store.rb
+++ config/initializers/google_token_store.rb
@@ -14,13 +14,3 @@ token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_STORE_PATH)
 
 # Use the token store in your authorization setup
 authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-
-# Function to obtain and store credentials
-def obtain_and_store_credentials(user_id, code)
-  credentials = authorizer.get_and_store_credentials_from_code(
-    user_id: user_id,
-    code: code,
-    base_url: 'http://localhost:5000/'
-  )
-  token_store.store_credentials(user_id, credentials)
-end
