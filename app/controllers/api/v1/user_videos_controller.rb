58eef6693f07074af22fbf4cd4743ef5e9180305
Steddman Bell <steddfast@gmail.com>
2024-06-02 20:12:25 -0700

    add some tests and start of happy path testing

--- app/controllers/api/v1/user_videos_controller.rb
+++ app/controllers/api/v1/user_videos_controller.rb
@@ -2,16 +2,23 @@ class API::V1::UserVideosController < ApplicationController
   before_action :set_user
 
   def create
+    begin
     @user_video = @user.user_videos.create(user_video_params)
 
     render json: { message: 'Video added to favorites' }, status: :created
+    rescue ActiveRecord::RecordNotFound => e
+      #render json: ErrorSerializer.new(ErrorMessage.new(e.message)).serialize_json, status :some status for record invalid
   end
 
   def destroy
+    begin
     @user_video = @user.user_videos.find(params[:id])
     @user_video.destroy
 
     render json: { message: 'Video removed from favorites' }, status: :ok
+    rescue ActiveRecord::ReecordNotFound => e
+      #render json: ErrorSerializer.new(ErrorMessage.new(e.message)).serialize_json, status :some status for record invalid
+
   end
 
   private
--- spec/requests/api/v1/user_videos_request_spec.rb
+++ spec/requests/api/v1/user_videos_request_spec.rb
@@ -2,6 +2,15 @@ require "rails_helper"
 
 RSpec.describe "API::V1::UserVideos", type: :request do
   before :each do
-    
+    @user1 = create(:user)
+    @video1 = create(:user_video, user: @user1)
+    @video2 = create(:user_video, user: @user1)
+    @video3 = create(:user_video, user: @user1)
+  end
+
+  describe "Happy paths" do
+    it "" do
+      
+    end
   end
 end
