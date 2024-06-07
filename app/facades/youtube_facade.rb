56ef74b388e539d15518a873dd257c02fb29654c
Nicholas Shanstrom <148294962+NicoShanstrom@users.noreply.github.com>
2024-06-07 04:54:54 -1000

    Update youtube_facade.rb
    took out pry

--- app/facades/youtube_facade.rb
+++ app/facades/youtube_facade.rb
@@ -1,6 +1,5 @@
 class YoutubeFacade
   def self.search(query_keywords, duration)
-    binding.pry
     youtube_videos = []
     youtube_service =|| YoutubeService.search(query_keywords, duration)
     youtube_videos = youtube_service.map do |video_data|
