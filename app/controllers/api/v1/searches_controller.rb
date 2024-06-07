b6ee285f2a4c72cde10a988c23dc102909997fa4
Steddman Bell <steddfast@gmail.com>
2024-06-06 17:32:05 -0700

    save point where backend is working correctly

--- app/controllers/api/v1/searches_controller.rb
+++ app/controllers/api/v1/searches_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::SearchesController < ApplicationController
   def index
-    binding.pry
     if params[:query].present? && params[:video_duration].present?
       query_keywords = params[:query]
       video_duration = params[:video_duration]
-      
-      @videos = YoutubeFacade.search(query_keywords, video_duration)
+      @videos = YoutubeService.search(query_keywords, video_duration)
       render json: @videos
     else
       render json: { error: "Query and video duration parameters are required" }, status: :bad_request
--- app/facades/youtube_facade.rb
+++ app/facades/youtube_facade.rb
@@ -1,7 +1,9 @@
 class YoutubeFacade
   def self.search(query_keywords, duration)
-    youtube_service = YoutubeService.search(query_keywords, duration)
-    youtube_service.map do |video_data|
+    binding.pry
+    youtube_videos = []
+    youtube_service =|| YoutubeService.search(query_keywords, duration)
+    youtube_videos = youtube_service.map do |video_data|
       YoutubeVideo.new(video_data)
     end
   end
