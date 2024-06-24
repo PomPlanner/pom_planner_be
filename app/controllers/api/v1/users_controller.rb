00fed8017a96b20653647cc3b647f4a3bf2584e0
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-06-24 11:26:35 -0400

    Clean up

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  # before_action :require_login, only: [:show]
-
   def show
     user = User.find(params[:id])
     render json: UserSerializer.new(user).serializable_hash
-  end
-
-  private
-
-  def require_login
-    unless session[:user_id].present?
-    # unless request.env["omniauth.auth"]
-      render json: { error: "You must be logged in to access this section" }, status: :unauthorized
-    end
   end
 end
