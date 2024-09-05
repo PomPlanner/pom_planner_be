fc1bbf477a0bb24f626fd8a906faf7be5f20a99a
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-09-05 17:27:35 -0400

    Updates event controller, google calendar serice to simplify creating the event

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,21 +1,10 @@
 class Api::V1::EventsController < ApplicationController
-
   def generate_google_calendar_link
     user = User.find(params[:user_id])
     video = user.user_videos.find(params[:video_id])
 
-    start_time = params[:start_time].to_datetime.utc
-    video_duration = video.duration
-
-    event_params = {
-      summary: params[:summary],
-      description: video.url,
-      start_time: start_time,
-      video_duration: video.duration
-    }
-
     google_calendar_service = GoogleCalendarService.new(user)
-    event_link = google_calendar_service.generate_event_link(event_params)
+    event_link = google_calendar_service.generate_event_link(video.url)
 
     render json: { event_link: event_link }
   rescue => e
--- app/services/google_calendar_service.rb
+++ app/services/google_calendar_service.rb
@@ -1,14 +1,9 @@
 require 'google/apis/calendar_v3'
 require 'googleauth'
-require 'googleauth/stores/file_token_store'
-require 'yaml'
 
 class GoogleCalendarService
-  include VideoMethods
-
   APPLICATION_NAME = 'PomPlanner'.freeze
   CREDENTIALS = { client_id: ENV['GOOGLE_CLIENT_ID'], client_secret: ENV['GOOGLE_CLIENT_SECRET'] }
-  TOKEN_PATH = 'token.yaml'.freeze
   SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_EVENTS
 
   def initialize(user)
@@ -20,65 +15,23 @@ class GoogleCalendarService
   def user_credentials(user)
     client_id = CREDENTIALS[:client_id]
     client_secret = CREDENTIALS[:client_secret]
-    token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
-    authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-    
-    access_token = @user.token
-    refresh_token = @user.refresh_token
-    
+
     credentials = Google::Auth::UserRefreshCredentials.new(
       client_id: client_id,
       client_secret: client_secret,
       scope: SCOPE,
-      refresh_token: refresh_token,
-      access_token: access_token,
-      additional_parameters: { 'access_type' => 'offline' }
+      refresh_token: @user.refresh_token,
+      access_token: @user.token
     )
-   
-    credentials.fetch_access_token! if credentials.expired?
 
+    credentials.fetch_access_token! if credentials.expired?
     credentials
   end
 
-  def create_event_with_video(summary, description, start_time, video_duration, video_url)
-    end_time = start_time + iso8601_duration_to_seconds(video_duration).seconds
-
-    event = Google::Apis::CalendarV3::Event.new(
-      summary: summary,
-      description: "#{description}\nWatch the video here: #{video_url}\nGet out of your seat!",
-      start: Google::Apis::CalendarV3::EventDateTime.new(date_time: start_time.utc.iso8601),
-      end: Google::Apis::CalendarV3::EventDateTime.new(date_time: end_time.utc.iso8601),
-      reminders: Google::Apis::CalendarV3::Event::Reminders.new(
-        use_default: false,
-        overrides: [
-          Google::Apis::CalendarV3::EventReminder.new(reminder_method: 'popup', minutes: 10),
-          Google::Apis::CalendarV3::EventReminder.new(reminder_method: 'popup', minutes: 0)
-        ]
-      )
-    )
-
-    @service.insert_event('primary', event)
-    event
-  end
-
-  def generate_event_link(event_params)
-    start_time = event_params[:start_time].to_datetime.utc.iso8601
-    video_duration = event_params[:video_duration]
-    end_time = calculate_end_time(event_params[:start_time].to_datetime.utc, video_duration).iso8601
-
-    event_description = "Watch this video: #{event_params[:description]}"
-
+  def generate_event_link(video_url)
     event_link = "https://www.google.com/calendar/render?action=TEMPLATE"
-    event_link += "&text=#{URI.encode_www_form_component(event_params[:summary])}"
-    event_link += "&details=#{URI.encode_www_form_component(event_description)}"
-    event_link += "&dates=#{start_time}/#{end_time}"
+    event_link += "&text=PomPlaner+Event"
+    event_link += "&details=Watch+this+video%3A+#{video_url}"
     event_link
-  end
-
-  private
-
-  def calculate_end_time(start_time, duration)
-    total_seconds = iso8601_duration_to_seconds(duration)
-    start_time + total_seconds.seconds
   end
 end
