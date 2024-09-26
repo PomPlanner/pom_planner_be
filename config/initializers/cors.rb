4592a627aa85a31a09dda15a52c6a857d324da3a
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-09-26 15:34:56 -0400

    Corrects urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://accounts.google.com", "https://pom-planner-6a8ebbf9e5c1.herokuapp.com/"
+    origins "http://localhost:3000", "https://accounts.google.com", "https://pom-planner.herokuapp.com"
 
     resource "*",
       headers: :any,
