b95a876d4d4095b6c33e153edf68c478bc893439
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-09-26 17:41:12 -0400

    Removes api/v1 from redirect_url for googoe_oauth2_url in google_oauth_service.rb

--- app/services/google_oauth_service.rb
+++ app/services/google_oauth_service.rb
@@ -1,7 +1,7 @@
 class GoogleOauthService
   def google_oauth2_url
     client_id = ENV['GOOGLE_CLIENT_ID']
-    redirect_uri = CGI.escape('https://pom-planner-6a8ebbf9e5c1.herokuapp.com/api/v1/auth/google_oauth2/callback')
+    redirect_uri = CGI.escape('https://pom-planner-6a8ebbf9e5c1.herokuapp.com/auth/google_oauth2/callback')
     scope = CGI.escape('email profile https://www.googleapis.com/auth/calendar.events')
     
     "https://accounts.google.com/o/oauth2/v2/auth?client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code&scope=#{scope}&prompt=select_account"
