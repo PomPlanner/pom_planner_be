eaac3af3e416cdf3ceef6001402aa11aa48551c4
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-23 15:19:01 -0400

    Modifies session_store.rb same_site: :none for session persistence across domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, tld_length: 2, secure: Rails.env.production? ? true : false, expire_after: 1.day
+Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, tld_length: 2, secure: Rails.env.production?, same_site: :none
 
