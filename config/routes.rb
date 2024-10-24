8eddf05648dec11052eba56d317f6f0283be5c6c
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-24 12:16:26 -0400

    Adds a root path to redirect to front end

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  root to: redirect('https://pom-planner-6a8ebbf9e5c1.herokuapp.com')  # Redirect root requests to the frontend
   namespace :api do
     namespace :v1 do
       post 'auth/google_oauth2', to: 'sessions#google_oauth2'
