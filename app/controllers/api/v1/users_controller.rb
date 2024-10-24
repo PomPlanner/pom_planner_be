695bf45d1bcf3f6f47b46863938051556b1f6b07
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-24 14:24:51 -0400

    debugs session for user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def show
     user = User.find(params[:id])
+    require 'pry'; binding.pry
     render json: UserSerializer.new(user).serializable_hash
   end
 end
