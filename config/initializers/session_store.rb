fefa715313aba906714214be951528868566b372
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-24 12:22:35 -0400

    Modifies session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, tld_length: 2, secure: true, same_site: :none
 # Rails.application.config.session_store :cookie_store, key: '_your_app_session', secure: Rails.env.production?, same_site: :none
-Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_your_app_session', domain: :all, secure: Rails.env.production?, tld_length: 2
 
