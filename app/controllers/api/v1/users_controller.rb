6320d85ea19945121ebaef9cc100262c109c7bf2
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-10-24 14:27:39 -0400

    Removes pry

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    require 'pry'; binding.pry
     render json: UserSerializer.new(user).serializable_hash
   end
 end
